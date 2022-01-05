d5de4ca7f54ddb27ac295005b2aa7dc5da618a08
Azhar Sajjad <iamazharsajjad@gmail.com>
2022-01-05 17:28:30 +0500

    Theme Start bootstap sb admin 2 intergrated

---
+++ app/javascript/vendor/index.js
@@ -0,0 +1,22 @@
+// import './sb-admin-2';
+
+// import './demo/chart-area-demo';
+// import './demo/chart-pie-demo';
+
+
+  import './vendor/jquery/jquery.min'
+
+  import './vendor/bootstrap/js/bootstrap.bundle.min'
+
+  //   <!-- Core plugin JavaScript-->
+  import './vendor/jquery-easing/jquery.easing.min'
+
+  //   <!-- Custom scripts for all pages-->
+  import './sb-admin-2.min'
+
+  //   <!-- Page level plugins -->
+  import './vendor/chart.js/Chart.min'
+
+  //   <!-- Page level custom scripts -->
+  import './demo/chart-area-demo'
+  import './demo/chart-pie-demo'

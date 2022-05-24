c350ea3bd7fd8377fc338e22b9dcdd2444b02238
Azhar Sajjad <iamazharsajjad@gmail.com>
2022-05-24 19:41:34 +0500

    WIP ..

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -7,5 +7,29 @@ class Admin
     def index
       @current_admin = current_admin.email
     end
+
+    def show
+      @admins = Admin.find(params[:id])
+    end
+
+    def edit
+      @admin = Admin.find(params[:id])
+    end
+
+    def update
+      @admin = Admin.find(params[:id])
+
+      if @admin.update(admin_params)
+        redirect_to department_admins_path(@admin)
+      else
+        render :edit
+      end
+    end
+
+    private
+
+      def admin_params
+        params.require(:admin).permit(:name)
+      end
   end
 end
